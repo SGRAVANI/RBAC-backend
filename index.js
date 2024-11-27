f4fff6e818a6e3cd78f2bc9a356a08207c2ce1ce
SGRAVANI <shakuntla.ravani@gmail.com>
2024-11-27 20:56:00 +0530

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import fileupload from "express-fileupload"
 import bodyParser from "body-parser";
 let app=express()
 dotenv.config()
-app.use(cors( {origin: "https://rbac-frontend-shakuntla.vercel.app/", // Your React app's URL
+app.use(cors( {origin:'https://rbac-frontend-shakuntla.vercel.app',
     credentials: true}))
 app.use(express.json({ limit: "10mb" }))
 app.use(cookieParser())
